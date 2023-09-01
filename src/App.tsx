70b45f91f26a882e816b9f5399a7094a02bf2d9d
alenagan <alena_gan@ukr.net>
2023-09-01 10:17:34 +0200

    start emotion

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="container mx-auto">
+    <h1 className="text-2xl text-emerald-700">Hello React</h1>
+    
+   
+    </div>
+  );
+}
+
+export default App;
