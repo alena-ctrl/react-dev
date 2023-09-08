9a4bbc33ad3976b9f190285dc9e51d885e16e7f6
romannikiforov <escaro@ukr.net>
2023-09-08 12:29:13 +0300

    fixes App.jsx

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-<<<<<<< HEAD
-import React from "react";
 import Counter from "./components/Counter";
 import Form from "./components/Form";
-=======
-import React from "react"
-import Counter from "./components/Counter"
-import Form from "./components/Form"
-import {Header1, Button, WrapButton} from './components/styles/style'
-import {WrapApp} from './components/styles/app'
+import { Header1, Button, WrapButton } from "./components/styles/style";
+import { WrapApp } from "./components/styles/app";
 
 function App() {
-  
   return (
-    <WrapApp >
-      <WrapButton >
-        <Counter> 
-          
-          {({count, increment, decrement}) => <>
-        
-      <Header1 color="red" >Counter: {count}</Header1>      
-      <Button onClick={increment} >Increment</Button>
-      <Button onClick={decrement} >Decrement</Button>      
-    </>
-        } 
+    <WrapApp>
+      <WrapButton>
+        <Counter>
+          {({ count, increment, decrement }) => (
+            <>
+              <Header1 color="red">Counter: {count}</Header1>
+              <Button onClick={increment}>Increment</Button>
+              <Button onClick={decrement}>Decrement</Button>
+            </>
+          )}
         </Counter>
       </WrapButton>
       <div className="row">
         <Form />
       </div>
-<<<<<<< HEAD
-    </div>
-  );
-=======
+      );
     </WrapApp>
-  )
->>>>>>> 4e7acbe1653b2516e96ca48e8a05fa7142a4d9a7
+  );
 }
 
 export default App;
--- src/components/styles/app.js
+++ src/components/styles/app.js
@@ -1,69 +1,73 @@
-import { jsx, keyframes } from '@emotion/react'
+import { jsx, keyframes } from "@emotion/react";
 
-import styled from '@emotion/styled/macro'
-import * as colors from './colors'
-import {FaSpinner} from 'react-icons/fa'
-import { AiOutlineMail } from "react-icons/ai"
+import styled from "@emotion/styled/macro";
+import * as colors from "./colors";
+import { FaSpinner } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 
-export const EmailIcons = styled(AiOutlineMail) `
-height: 24px; 
-width: 24px; 
-color: red;
+export const EmailIcons = styled(AiOutlineMail)`
+  height: 24px;
+  width: 24px;
+  color: red;
 `;
 
-const spin = keyframes ({
-    "0%": {transform: 'rotate(0deg)'},
-    "100%": {transform: 'rotate(360deg)'},   
-
+const spin = keyframes({
+  "0%": { transform: "rotate(0deg)" },
+  "100%": { transform: "rotate(360deg)" },
 });
 
 export const Spiner = styled(FaSpinner)({
-    animation:`${spin} 1s linear infinite`
-})
+  animation: `${spin} 1s linear infinite`,
+});
 
 export function FullSpinner() {
-    return <div style={{
-        fontSize:"4rem",
+  return (
+    <div
+      style={{
+        fontSize: "4rem",
         height: "4rem",
-        display:"flex",
+        display: "flex",
         flexDirection: "column",
-        alignItems:"center",
-    }}>
-        <Spiner />
+        alignItems: "center",
+      }}
+    >
+      <Spiner />
     </div>
+  );
 }
 
-export const WrapApp = styled.div `
-max-width: 1200px;
-margin: 1rem auto 0;
+export const WrapApp = styled.div`
+  max-width: 1200px;
+  margin: 1rem auto 0;
 `;
 
 export const WrapForm = styled.form({
-    maxWidth: "400px",
-    margin: "1rem auto",
-}) ;
+  maxWidth: "400px",
+  margin: "1rem auto",
+});
 
 export const FormGroup = styled.div({
-    marginBottom: "1rem",
-}) ;
+  marginBottom: "1rem",
+});
 
 export const Label = styled.label({
-    display: "block",
-    marginBottom: "1rem",
-}) ;
+  display: "block",
+  marginBottom: "1rem",
+});
 
-export const Input= styled.input({
-    display: "block",
-    width: "100%",
-    padding: "0.6rem",
-    marginBottom: "1rem",
-}) ;
+export const Input = styled.input({
+  display: "block",
+  width: "100%",
+  padding: "0.6rem",
+  marginBottom: "1rem",
+});
 
-export const ButtonGroup= styled.div({
-    padding: "0.5rem 0"
-}) ;
+export const ButtonGroup = styled.div({
+  padding: "0.5rem 0",
+});
 
-export const Button= styled.button({
+export const Button = styled.button(
+  {
     padding: ".4rem .8rem",
     textTransform: "uppercase",
     borderColor: "transparent",
@@ -71,20 +75,17 @@ export const Button= styled.button({
     cursor: "pointer",
     color: "#ffffff",
 
+    "&:hover": {
+      backgroundColor: "#ff2b56",
+    },
+  },
 
-"&:hover": {
-    backgroundColor: "#ff2b56"
-},
-},
-
-({mr=0, type}) => ({
+  ({ mr = 0, type }) => ({
     marginRight: mr,
     backgroundColor: type === "reset" ? colors.danger : colors.primary,
     "&:hover": {
-        backgroundColor: type === "reset" ? colors.dangerHover : colors.primaryHover
-       } 
-}),
-
-
-
-) ;
+      backgroundColor:
+        type === "reset" ? colors.dangerHover : colors.primaryHover,
+    },
+  })
+);
